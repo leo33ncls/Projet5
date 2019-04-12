b3911e95e026c7f0e747f49cdfcfd0af65e0f3d2
Léo Nicolas <leoncls3398@gmail.com>
2019-04-12 21:17:10 +0200

    Ajout du UIImagePickerController

--- Projet_5/Controller/ViewController.swift
+++ Projet_5/Controller/ViewController.swift
@@ -8,10 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var rectangleImagesView: RectangleImagesView!
     @IBOutlet var imagesView: [UIImageView]!
+    
+    var imagePicker: UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +29,26 @@ class ViewController: UIViewController {
         imageView.addGestureRecognizer(tapGestureRecognizer)
     }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        imagePicker?.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        imagesView[0].image = image
+        imagesView[0].contentMode = .scaleToFill
+        imagePicker?.dismiss(animated: true, completion: nil)
+    }
+    
     @objc func choiceImage() {
+        imagePicker = UIImagePickerController()
+        imagePicker?.delegate = self
+        
+        guard imagePicker != nil else { return }
+        imagePicker?.sourceType = .photoLibrary
+        self.present(self.imagePicker!, animated: true, completion: nil)
     }
 
     @IBAction func tapButton1(_ sender: Any) {
